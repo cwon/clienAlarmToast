e84abb03da91b4ed12dd4d7d95101e97c8be0071
Won cheehun <dream40@nate.com>
2020-07-13 20:49:16 +0900

    Bug Fix: Too high cpu usage when stop status and Add Time Interval Interface and function

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
     "image"
     "time"
     "io"
+    "fmt"
+    "strconv"
     "encoding/hex"
     "crypto/md5"
 
@@ -23,6 +25,7 @@ var (
     euckrEnc = korean.EUCKR.NewEncoder()
     userId nucular.TextEditor
     passEd nucular.TextEditor
+    intervalEd nucular.TextEditor
     start bool
     failReason string
     alarmList map[string]bool
@@ -32,21 +35,34 @@ func main() {
     passEd.PasswordChar = '*'
     passEd.Flags = nucular.EditField
 
+    intervalEd.Filter = nucular.FilterDecimal
+    intervalEd.Buffer = []rune(fmt.Sprintf("%d", 15))
+
     start = false
     alarmList = make(map[string]bool)
 
-    wnd := nucular.NewMasterWindowSize(0, "ClienAlarm", image.Point{200,138} ,updatefn)
+    wnd := nucular.NewMasterWindowSize(0, "ClienAlarm", image.Point{200,200} ,updatefn)
     wnd.SetStyle(style.FromTheme(style.DarkTheme, 2.0))
 
     go func() {
         for {
                 if start == true {
-                    getNewAlaram() 
-                    if start == false {
+                    var ret bool
+                    ret, failReason = getNewAlaram() 
+                    if ret == false {
                         wnd.PopupOpen("Error", nucular.WindowMovable|nucular.WindowTitle|nucular.WindowDynamic|nucular.WindowNoScrollbar, rect.Rect{50, 20, 110, 100}, true, errorPopup)                        
+                        start = false
                     }                            
-                }                
-                time.Sleep(10 * time.Second)
+                    interval, _ := strconv.Atoi(string(intervalEd.Buffer))
+                    if interval > 0 {
+                        time.Sleep(time.Duration(interval) * time.Second)
+                        fmt.Println(interval)                
+                    } else {
+                        start = false
+                    }                    
+                } else {
+                    time.Sleep(time.Duration(1) * time.Second)
+                }     
         }
     }();
 
@@ -61,20 +77,26 @@ func updatefn(w *nucular.Window) {
 
     idLabel, _ := euckrEnc.String("ID")
     pwLabel, _ := euckrEnc.String("PW")
+    intervalLabel, _ := euckrEnc.String("Interval-sec")
+    runningLabel, _ := euckrEnc.String("Running!")
 
     w.Row(20).Dynamic(1)
 
-    w.Label(idLabel, "LC")
-    userId.Edit(w)
-
-    w.Label(pwLabel, "LC")
-    passEd.Edit(w) 
-
     if start == false {
+        w.Label(idLabel, "LC")
+        userId.Edit(w)
+
+        w.Label(pwLabel, "LC")
+        passEd.Edit(w)
+
+        w.Label(intervalLabel, "LC")
+        intervalEd.Edit(w)
+
         if w.ButtonText("Start") {
             start = true
         }  
     } else {
+        w.Label(runningLabel, "LC")
         if w.ButtonText("Stop") {
             start = false
         }  
@@ -82,7 +104,7 @@ func updatefn(w *nucular.Window) {
     }
 }
 
-func getNewAlaram() {   
+func getNewAlaram() (bool, string) {   
     c := colly.NewCollector()
     csrf := ""
 
@@ -95,9 +117,8 @@ func getNewAlaram() {
     if csrf != "" { 
         login_collector := c.Clone()
         err := login_collector.Post("https://www.clien.net/service/login", map[string]string{"_csrf": csrf, "userId": string(userId.Buffer), "userPassword": string(passEd.Buffer)})
-        if err != nil {
-            start = false;
-            failReason = "Network error"
+        if err != nil {            
+            return false, "Network error";
         }
 
         // start scraping
@@ -126,7 +147,7 @@ func getNewAlaram() {
                             {"protocol", sMenu, "https://www.clien.net/service/board/" + Split[0] + "/" + Split[1]},
                            },
                     }
-                                       
+
                     notification.Push()
                     alarmList[key] = true
                 }        
@@ -135,21 +156,28 @@ func getNewAlaram() {
             }
         })
 
+        alarm_page_result := 0
         alarm_collector.OnResponse(func(r *colly.Response) {
             if r.StatusCode != 200 {
-                start = false
-                failReason = "Network error"
+                alarm_page_result = 1
             } 
             if strings.Contains(string(r.Body), "self.close()") {
-                start = false
-                failReason = "Login error"
+                alarm_page_result = 2
             }
         })
         alarm_collector.Visit("https://www.clien.net/service/getAlarmList")
+        if alarm_page_result == 1 {
+            return false, "Network error"         
+        } 
+
+        if alarm_page_result == 2 {
+            return false, "Login error"
+        }
     } else {
-        start = false;
-        failReason = "Network error"
+        return false, "Network error" 
     }
+
+    return true, "OK"
 }
 
 func errorPopup(w *nucular.Window) {
